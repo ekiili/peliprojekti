2bd7a064173c3024d587f02a66e3959cf82e257e
Roope Autio <roope.oskari.autio@gmail.com>
2024-02-22 14:50:41 +0200

    Flash kinda work
    Santeri fixing it so we can edit flashing speed

--- Assets/Code/Health.cs
+++ Assets/Code/Health.cs
@@ -6,19 +6,41 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float _health = 3f;
     [SerializeField] private float _invincibilityTime = 2f;
+    [SerializeField] private float _timeSinceChange = 0.75f;
+    SpriteRenderer _spriteRenderer;
+    private bool _isVisible = true;
     private bool _isInvincible = false;
+
+    public void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void FixedUpdate()
     {
         if (_isInvincible)
         {
             _invincibilityTime -= Time.deltaTime;
-            if (_invincibilityTime <= 0)
+            if (_invincibilityTime >= 0 && _isVisible)
             {
+                _spriteRenderer.enabled = false;
+                _isVisible = false;
+            }
+            else if (_invincibilityTime >= 0 && !_isVisible)
+            {
+                _spriteRenderer.enabled = true;
+                _isVisible = true;
+            }
+            else if (_invincibilityTime <= 0)
+            {
+                Debug.Log("Player is no longer invincible");
                 _isInvincible = false;
+                _spriteRenderer.enabled = true;
+                _isVisible = true;
                 _invincibilityTime = 2f;
             }
         }
+
     }
     public void TakeDamage(float _damage)
     {
