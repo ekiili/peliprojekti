3557596ae8077417e5eacce43ae7aea12c9b7fb7
MacAutio <roope.oskari.autio@gmail.com>
2024-03-19 14:04:35 +0200

    Mobile joystick working
    known bug:
    if finger moves over the joystick area, knob goes back to the center and movement stops. Might have a solution in mind to fix this

---
+++ Assets/Code/PlayerTouchMovement.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlayerTouchMovement : MonoBehaviour
+{
+    [SerializeField] private Vector2 JoystickSize = new Vector2(300, 300);
+    [SerializeField] private FloatingJoystick Joystick;
+    [SerializeField] private Rigidbody2D _rb = null;
+    [SerializeField] private float _speed = 1f;
+
+    private Finger MovementFinger;
+    private Vector2 MovementAmount;
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += Touch_onFingerDown;
+        ETouch.Touch.onFingerUp += Touch_onFingerUp;
+        ETouch.Touch.onFingerMove += Touch_onFingerMove;
+    }
+    private void OnDisable()
+    {
+        ETouch.Touch.onFingerDown -= Touch_onFingerDown;
+        ETouch.Touch.onFingerUp -= Touch_onFingerUp;
+        ETouch.Touch.onFingerMove -= Touch_onFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+    private void Touch_onFingerMove(Finger MovedFinger)
+    {
+        if (MovedFinger == MovementFinger)
+        {
+            Vector2 knobPosition;
+            float maxMovement = JoystickSize.x / 2f;
+            ETouch.Touch currentTouch = MovedFinger.currentTouch;
+
+            if (Vector2.Distance(currentTouch.screenPosition,Joystick.RectTransform.anchoredPosition) > maxMovement)
+            {
+                knobPosition = (currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition).normalized / maxMovement;
+            }
+            else
+            {
+                knobPosition = currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition;
+            }
+            Joystick.Knob.anchoredPosition = knobPosition;
+            MovementAmount = knobPosition * maxMovement;
+        }
+    }
+
+    private void Touch_onFingerUp(Finger LostFinger)
+    {
+        if (LostFinger == MovementFinger)
+        {
+            MovementFinger = null;
+            Joystick.Knob.anchoredPosition = Vector2.zero;
+            Joystick.gameObject.SetActive(false);
+            MovementAmount = Vector2.zero;
+        }
+    }
+
+    private void Touch_onFingerDown(Finger TouchedFinger)
+    {
+        if (MovementFinger == null)
+        {
+            MovementFinger = TouchedFinger;
+            MovementAmount = Vector2.zero;
+            Joystick.gameObject.SetActive(true);
+            Joystick.RectTransform.sizeDelta = JoystickSize;
+            Joystick.RectTransform.anchoredPosition = TouchedFinger.screenPosition;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 scaledMovement = _speed * Time.deltaTime * new Vector3(MovementAmount.x, MovementAmount.y, 0);
+        _rb.AddForce(scaledMovement * 0.01f);
+        if (scaledMovement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, scaledMovement);
+        }
+    }
+}
