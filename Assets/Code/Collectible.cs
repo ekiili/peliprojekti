f1b067fabd44b71fd4dd91a1cf15802e324d1505
Roope Autio <roope.oskari.autio@gmail.com>
2024-03-25 13:49:46 +0200

    Ground no eat alive fish
    Vittu saatana

--- Assets/Code/Collectible.cs
+++ Assets/Code/Collectible.cs
@@ -14,9 +14,11 @@ public class Collectible : MonoBehaviour
     [SerializeField] private TrashType _trashType = TrashType.metal;
     [SerializeField] public float _value = 1f;
 
+    public bool _col = true;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<Collector>() != null)
+        if (other.gameObject.GetComponent<Collector>() != null && _col)
         {
             bool collected = other.gameObject.GetComponent<Collector>().CollectedTrash(_trashType, _value);
             if (collected)
