8951523ce1f667503e150c95b07417e1b75abac0
sjkorpela <santeri.j.korpela03@gmail.com>
2024-04-08 14:10:40 +0300

    Updated fishes

--- Assets/Code/Fish.cs
+++ Assets/Code/Fish.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
-    private Collectible _cl;
+    // private Collectible _cl;
+    [SerializeField] private GameObject _deadFish;
+    [SerializeField] private bool _flipSprite;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
-        _cl = GetComponent<Collectible>();
-        _cl._col = false;
+        // _cl = GetComponent<Collectible>();
+        // _cl._col = false;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -30,10 +33,16 @@ public class Fish : MonoBehaviour
     }
     void Die()
     {
-        _rb.velocity = Vector2.zero;
-        _rb.gravityScale = 0.01f;
-        _sr.flipY = true;
-        transform.gameObject.tag = "DeadFish";
-        _cl._col = true;
+        // _rb.velocity = Vector2.zero;
+        // _rb.gravityScale = 0.01f;
+        // _sr.flipY = true;
+        // transform.gameObject.tag = "DeadFish";
+        // _cl._col = true;
+        GameObject _dead = Instantiate(_deadFish, transform.position, transform.rotation);
+        _dead.GetComponent<Rigidbody2D>().gravityScale = 0.01f;
+        if (_flipSprite) {
+            _dead.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        Destroy(this.gameObject);
     }
 }
