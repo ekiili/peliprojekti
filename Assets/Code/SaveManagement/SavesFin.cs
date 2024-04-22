6070cd29c6ccc7c8eb6a4a66fd650c92d6df9f18
MacAutio <roope.oskari.autio@gmail.com>
2024-04-23 00:27:09 +0300

    JosTääPaskaEiToimiPommitanUnitynHQ:n

---
+++ Assets/Code/SaveManagement/SavesFin.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using TMPro;
+
+public class SavesFin : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    private static int _highScore;
+
+    void OnApplicationQuit()
+    {
+        HighScore = GameManager.Score;
+    }
+    private void Awake()
+    {
+        if (_scoreText == null)
+        {
+            _scoreText = GetComponent<TextMeshProUGUI>();
+        }
+    }
+    void FixedUpdate()
+    {
+        _scoreText.text = $"Parhaat Pisteet: {HighScore}";
+    }
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            Debug.Log("Game Started!");
+            _highScore = PlayerPrefs.GetInt("HighScore");
+            Debug.Log("Score loaded: " + GameManager.Score);
+        }
+    }
+
+    public void SaveGame() {
+        HighScore = GameManager.Score;
+    }
+
+    public static int HighScore
+    {
+        get => _highScore;
+        set
+        {
+            if (value > _highScore)
+            {
+                _highScore = value;
+                PlayerPrefs.SetInt("HighScore", _highScore);
+            }
+        }
+    }
+}
